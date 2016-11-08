5d06540337abffd9cffebee1eca4a979bee41a09
lcomstive <lcomstive@hotmail.com>
2016-11-08 11:37:00 +0800

    Uploaded encryption and compression templates

---
+++ Servers/RTNet_Server/src/rt_compression.cpp
@@ -0,0 +1,14 @@
+#include "rt_compression.h"
+
+using namespace RTNet;
+
+rt_byte* RTCompression::Compress(rt_byte* input, int length)
+{
+    return nullptr;   
+}
+
+rt_byte* RTCompression::Decompress(rt_byte* input, int length, int* outLength)
+{
+    outLength = 0;
+    return nullptr;   
+}
---
+++ Servers/RTNet_Server/src/rt_encryption.cpp
@@ -0,0 +1,14 @@
+#include "rt_encryption.h"
+
+using namespace RTNet;
+
+rt_byte* RTEncryption::Encrypt(rt_byte* input, int length)
+{
+    return nullptr;   
+}
+
+rt_byte* RTEncryption::Encrypt(rt_byte* input, int length, int* outLength)
+{
+    outLength = 0;
+    return nullptr;   
+}
