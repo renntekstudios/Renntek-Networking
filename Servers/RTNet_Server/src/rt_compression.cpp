a53f93ee7873a8b555ff3b74252ef99017266721
lcomstive <lcomstive@hotmail.com>
2016-11-08 11:44:47 +0800

    Update rt_compression.cpp

--- Servers/RTNet_Server/src/rt_compression.cpp
+++ Servers/RTNet_Server/src/rt_compression.cpp
@@ -2,13 +2,14 @@
 
 using namespace RTNet;
 
-rt_byte* RTCompression::Compress(rt_byte* input, int length)
+rt_byte* RTCompression::Compress(rt_byte* input, int length, int* outLength)
 {
-    return nullptr;   
+    *outLength = length;
+    return input;
 }
 
 rt_byte* RTCompression::Decompress(rt_byte* input, int length, int* outLength)
 {
-    outLength = 0;
-    return nullptr;   
+    *outLength = length;
+    return input;   
 }
