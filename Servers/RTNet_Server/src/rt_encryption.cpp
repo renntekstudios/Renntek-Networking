a2d3a475e775e032fae14d8c56b4e910a0e1c571
lcomstive <lcomstive@hotmail.com>
2016-11-08 11:45:21 +0800

    Update rt_encryption.cpp

--- Servers/RTNet_Server/src/rt_encryption.cpp
+++ Servers/RTNet_Server/src/rt_encryption.cpp
@@ -2,13 +2,14 @@
 
 using namespace RTNet;
 
-rt_byte* RTEncryption::Encrypt(rt_byte* input, int length)
+rt_byte* RTEncryption::Encrypt(rt_byte* input, int length, int* outLength)
 {
-    return nullptr;   
+    *outLength = length;
+    return input;
 }
 
 rt_byte* RTEncryption::Encrypt(rt_byte* input, int length, int* outLength)
 {
-    outLength = 0;
-    return nullptr;   
+    *outLength = length;
+    return input;
 }
